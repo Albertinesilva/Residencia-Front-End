fc8491b992cd471585c87fa67205560815f5fb13
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2024-04-23 23:34:48 -0300

    app.routes e appcomponents

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  
+  {
+    path: '',
+    loadComponent: () => import('./components/items-list/items-list.component').then(c => c.ItemsListComponent)
+  },
+  {
+    path: 'carrinho',
+    loadComponent: () => import('./components/cart/cart.component').then(c => c.CartComponent)
+  }
+];
